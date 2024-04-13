874d7153da1d7c633f85dc027850e07c991bc73c
rawift <rawatabhiips@gmail.com>
2024-04-13 14:31:59 +0530

    six

--- src/ip.js
+++ src/ip.js
@@ -1,4 +1,4 @@
-export const server_ip = "https://www.frenz.top/server1"
+export const server_ip = "https://www.frenz.top"
 export const socket_ip = "https://www.frenz.top/server2"
 export const socket_ip2 = "hhttps://www.frenz.top/server3"
 
