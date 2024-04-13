e174402c9328111c0c3afb2573004449b4e56a16
rawift <rawatabhiips@gmail.com>
2024-04-13 15:54:05 +0530

    six

--- src/ip.js
+++ src/ip.js
@@ -1,5 +1,5 @@
 export const server_ip = "https://www.frenz.top"
-export const socket_ip = "https://www.frenz.top/server2"
-export const socket_ip2 = "hhttps://www.frenz.top/server3"
+export const socket_ip = "wss://www.frenz.top/server2"
+export const socket_ip2 = "wss://www.frenz.top/server3"
 
 
