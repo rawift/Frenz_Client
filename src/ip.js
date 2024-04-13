2d9b139f7cfbc57edfa567d563aae270a64c2a95
rawift <rawatabhiips@gmail.com>
2024-04-13 18:24:42 +0530

    six

--- src/ip.js
+++ src/ip.js
@@ -1,5 +1,5 @@
 export const server_ip = "https://www.frenz.top"
-export const socket_ip = "wss://www.frenz.top/server2"
+export const socket_ip = " https://23loohl39b.execute-api.ap-south-1.amazonaws.com/default/Frenz_socket"
 export const socket_ip2 = "wss://www.frenz.top/server3"
 
 
