8278c3846a1e6f98e918e85ed8005ee0279ae450
rawift <rawatabhiips@gmail.com>
2024-04-14 21:43:36 +0530

    six

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { Link } from 'react-router-dom';
 import buttonImage5 from '../images/instagram.png';
 import Bottom from '../components/Bottom/Bottom';
@@ -61,3 +61,60 @@ const Info = () => {
 };
 
 export default Info;
+*/
+import React from 'react';
+import { Link } from 'react-router-dom';
+import buttonImage5 from '../images/instagram.png';
+import Bottom from '../components/Bottom/Bottom';
+
+const Info = () => {
+  return (
+    <>
+      <div className="bg-black h-screen text-white">
+        <div className="w-100 p-8 overflow-auto" style={{ maxHeight: "calc(100% - 3.5rem)" }}>
+          <h2 className="text-2xl font-bold mb-4">About app</h2>
+          <p>New matches automatically made every day at 9 AM</p>
+          <p>You must submit the form to get the matches (Gender can't be updated). Refresh after submission</p>
+          <p>Use voice to instantly talk to a random match.</p>
+          <p>Chats automatically get deleted at 9 AM</p>
+          <p>App is running in Beta version, bugs and glitches are there</p>
+          <p>App is not responsive (Install react-app from the browser by clicking on three dots)</p>
+          <Link to="/login"><p>If any bug occurs, relogin by clicking on me!</p></Link>
+          <p>New features coming soon</p>
+          <p>Let me know any recommendations from you guys</p>
+          <p>Please be respectful towards each other on both chats and audio calls</p>
+          <p>Allow the audio permission to use Voice</p>
+          <p>Sometimes chats may not be in real-time, relogin/reload to resolve</p>
+          <a href='https://forms.gle/EQgbdEizsfCiohPU8'>Delete Account Click on me!</a>
+
+          {/* Add a two-line gap */}
+          <div style={{ height: "2rem" }}></div>
+
+          <h2 className="text-2xl font-bold mb-4">Developed by</h2>
+          <ul>
+            <li className='flex items-center'> 
+              <div>
+                Abhishek Singh 
+              </div>
+              <a href="https://www.instagram.com/abhi___rawat/">
+                <img src={buttonImage5} className='w-10 mx-2' alt="Icon" />
+              </a>
+            </li>
+            <li className='flex items-center'> 
+              <div>
+                Prem Singh
+              </div>
+              <a href="https://www.instagram.com/._p.r.e.m._/">
+                <img src={buttonImage5} className='w-10 mx-2' alt="Icon" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <Bottom />
+    </>
+  );
+};
+
+export default Info;
+
